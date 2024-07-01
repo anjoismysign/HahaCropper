f9f28fa19ee40b4634b9905d0231e4ad3ecc2971
lbenav8095 <lubensaico@gmail.com>
2024-06-30 20:11:22 -0600

    Fixes Cropper

--- src/main/java/me/anjoismysign/cropper/Cropper.java
+++ src/main/java/me/anjoismysign/cropper/Cropper.java
@@ -5,6 +5,7 @@ import me.anjoismysign.anjo.swing.components.AnjoComboBox;
 import me.anjoismysign.anjo.swing.components.AnjoTextField;
 import me.anjoismysign.anjo.swing.listeners.TextInputType;
 import me.anjoismysign.hahaswing.BubbleFactory;
+import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -12,6 +13,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
@@ -23,9 +25,7 @@ public class Cropper {
 
     private static void openCropper() {
         Uber<String> sizeLabel = Uber.drive(" (drag a file first)");
-        Uber<Image> toCrop = Uber.fly();
-        Uber<Integer> toCropSize = Uber.drive(0);
-        Uber<String> toCropFileName = Uber.fly();
+        Uber<List<File>> toCrop = Uber.drive(new ArrayList<>());
         AnjoTextField elements = AnjoTextField.build("Elements " + sizeLabel.thanks())
                 .addColorToText(TextInputType.INTEGER, Color.RED, false)
                 .addColorToText(TextInputType.INTEGER, Color.BLACK, true);
@@ -45,30 +45,25 @@ public class Cropper {
                                 .getImage().getScaledInstance(256, 256, Image.SCALE_SMOOTH),
                         true,
                         file -> {
+                            boolean isDirectory = file.isDirectory();
                             boolean isPng = file.getName().endsWith(".png");
                             if (!isPng) {
+                                if (isDirectory) {
+                                    File[] pictures = file.listFiles((dir, name) -> name.endsWith("png"));
+                                    toCrop.talk(pictures == null ? new ArrayList<>() : List.of(pictures));
+                                    return;
+                                }
                                 JOptionPane.showMessageDialog(null, "Only PNG files are supported.", "Error", JOptionPane.ERROR_MESSAGE);
                                 return;
                             }
-                            Image image = new ImageIcon(file.getAbsolutePath()).getImage();
-                            int width = image.getWidth(null);
-                            int height = image.getHeight(null);
-                            if (width != height) {
-                                JOptionPane.showMessageDialog(null, "Only square images are supported.", "Error", JOptionPane.ERROR_MESSAGE);
-                                return;
-                            }
-                            toCropSize.talk(width);
-                            toCropFileName.talk(file.getName().replace(".png", "") + "-cropped.png");
-                            sizeLabel.talk("Elements (size: " + width + ")");
-                            elements.getLabel().setText(sizeLabel.thanks());
-                            toCrop.talk(image);
+                            toCrop.talk(List.of(file));
                         },
                         elements,
                         AnjoComboBox.build("Split", List.of("12x", "16x", "32x")))
                 .onBlow(anjoPane -> {
-                    Image image = toCrop.thanks();
-                    if (image == null) {
-                        JOptionPane.showMessageDialog(null, "No image was selected.", "Error", JOptionPane.ERROR_MESSAGE);
+                    List<File> croppable = toCrop.thanks();
+                    if (croppable == null || croppable.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "No file were selected.", "Error", JOptionPane.ERROR_MESSAGE);
                         System.exit(0);
                         return;
                     }
@@ -87,63 +82,72 @@ public class Cropper {
                         System.exit(0);
                         return;
                     }
-
-                    // Convert Image to BufferedImage
-                    BufferedImage sourceBufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-                    Graphics graphics = sourceBufferedImage.getGraphics();
-                    graphics.drawImage(image, 0, 0, null);
-                    graphics.dispose();
-
-                    size = split * size;
-                    System.out.println(size);
-
-                    BufferedImage destinationBufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
-                    Graphics2D graphics2D = destinationBufferedImage.createGraphics();
-                    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-                    graphics2D.drawImage(sourceBufferedImage, 0, 0, size, size, null);
-                    graphics2D.dispose();
-
-                    if (size > split) {
-                        int rows = size / split; // The values for rows and cols would depend on your image's pixel size
-                        int cols = size / split;
-                        int count = 1;
-                        File downloadsFolder = new File(System.getProperty("user.home") + "/Downloads");
-                        File outputFolder = new File(downloadsFolder, toCropFileName.thanks().replace(".png", ""));
-                        outputFolder.mkdirs();
-                        for (int x = 0; x < rows; x++) {
-                            for (int y = 0; y < cols; y++) {
-                                // Initialize the image array with image chunks
-                                BufferedImage img = new BufferedImage(split, split, destinationBufferedImage.getType());
-
-                                // draws the image chunk
-                                Graphics2D gr = img.createGraphics();
-                                gr.drawImage(destinationBufferedImage, 0, 0, split, split, split * y, split * x, split * y + split, split * x + split, null);
-                                gr.dispose();
-
-                                // Write the BufferedImage to a file in the Downloads folder
-                                File outputFile = new File(outputFolder, count + ".png");
-                                try {
-                                    ImageIO.write(img, "png", outputFile);
-                                } catch (IOException e) {
-                                    JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                                }
-                                count++;
-                            }
-                        }
-                        openCropper();
-                    } else {
-                        // Write the BufferedImage to a file in the Downloads folder
-                        File downloadsFolder = new File(System.getProperty("user.home") + "/Downloads");
-                        File outputFile = new File(downloadsFolder, toCropFileName.thanks());
-                        try {
-                            ImageIO.write(destinationBufferedImage, "png", outputFile);
-                            openCropper();
-                        } catch (IOException e) {
-                            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                            System.exit(0);
-                            return;
-                        }
-                    }
+                    System.out.println("Cropping " + croppable.size() + " file(s) with size " + size + " and split " + split + "x");
+                    croppable.forEach(file -> {
+                        cropImage(file, size, split);
+                    });
+                    openCropper();
                 });
+    }
+
+    private static void cropImage(@NotNull File file,
+                                  int size,
+                                  int split) {
+        Image image = new ImageIcon(file.getAbsolutePath()).getImage();
+        final String fileName = file.getName();
+        System.out.println(file.getPath());
+        // Convert Image to BufferedImage
+        BufferedImage sourceBufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics graphics = sourceBufferedImage.getGraphics();
+        graphics.drawImage(image, 0, 0, null);
+        graphics.dispose();
+
+        size = split * size;
+
+        BufferedImage destinationBufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics2D = destinationBufferedImage.createGraphics();
+        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+        graphics2D.drawImage(sourceBufferedImage, 0, 0, size, size, null);
+        graphics2D.dispose();
+
+        if (size > split) {
+            int rows = size / split; // The values for rows and cols would depend on your image's pixel size
+            int cols = size / split;
+            int count = 1;
+            File downloadsFolder = new File(System.getProperty("user.home") + "/Downloads");
+            File outputFolder = new File(downloadsFolder, fileName.replace(".png", ""));
+            outputFolder.mkdirs();
+            for (int x = 0; x < rows; x++) {
+                for (int y = 0; y < cols; y++) {
+                    // Initialize the image array with image chunks
+                    BufferedImage img = new BufferedImage(split, split, destinationBufferedImage.getType());
+
+                    // draws the image chunk
+                    Graphics2D gr = img.createGraphics();
+                    gr.drawImage(destinationBufferedImage, 0, 0, split, split, split * y, split * x, split * y + split, split * x + split, null);
+                    gr.dispose();
+
+                    // Write the BufferedImage to a file in the Downloads folder
+                    File outputFile = new File(outputFolder, count + ".png");
+                    try {
+                        ImageIO.write(img, "png", outputFile);
+                    } catch (IOException e) {
+                        JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                    count++;
+                }
+            }
+        } else {
+            // Write the BufferedImage to a file in the Downloads folder
+            File downloadsFolder = new File(System.getProperty("user.home") + "/Downloads");
+            File outputFile = new File(downloadsFolder, fileName);
+            try {
+                ImageIO.write(destinationBufferedImage, "png", outputFile);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+                return;
+            }
+        }
     }
 }
